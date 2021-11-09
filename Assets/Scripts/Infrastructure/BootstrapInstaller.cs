866840106bb8d873395ba79327880c95ac261f55
Nikita Misko <miskonm@gmail.com>
2021-11-09 13:39:35 +0300

    !!! Start working on HOMETASK 3 !!!

--- Assets/Scripts/Infrastructure/BootstrapInstaller.cs
+++ Assets/Scripts/Infrastructure/BootstrapInstaller.cs
@@ -1,17 +1,27 @@
+using Course.Managers.LocalizationManager;
+using Course.Managers.SceneManager;
+using Course.Managers.TimeScaleManager;
 using RolePlayer.Infrastructure.AssetManagement;
 using RolePlayer.SaveLoad;
 using RolePlayer.UI.WindowService;
+using UnityEngine;
 using Zenject;
 
 namespace RolePlayer.Infrastructure
 {
   public class BootstrapInstaller : MonoInstaller
   {
+    public GameObject LocalizationManagerPrefab;
+    
     public override void InstallBindings()
     {
       InstallAssetProvider();
       InstallSaveLoadService();
       InstallUiWindowService();
+
+      InstallLocalization();
+      InstallSceneManager();
+      InstallTimeManager();
     }
 
     private void InstallAssetProvider()
@@ -29,6 +39,22 @@ namespace RolePlayer.Infrastructure
     {
       Container.Bind<IUIFactory>().To<UIFactory>().AsSingle();
       Container.Bind<IWindowService>().To<WindowService>().AsSingle();
+    }
+
+    private void InstallLocalization()
+    {
+      Container.Bind<ILocalizationManager>().To<LocalizationManager>()
+       .FromComponentInNewPrefab(LocalizationManagerPrefab).AsSingle();
+    }
+
+    private void InstallSceneManager()
+    {
+      Container.Bind<ISceneManager>().To<SceneManager>().FromNewComponentOnNewGameObject().AsSingle();
+    }
+
+    private void InstallTimeManager()
+    {
+      Container.Bind<ITimeScaleManager>().To<TimeScaleManager>().FromNewComponentOnNewGameObject().AsSingle();
     }
   }
 }
---
+++ Assets/Scripts/UI/Menu/MenuWindow.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using Course.Base;
+using Course.Managers.SceneManager;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace RolePlayer
+{
+  public class MenuWindow : AbstractBehaviour, IOnBeginSceneLoad
+  {
+    public Button PlayButton;
+    public GameObject LoadingBar;
+
+    private ISceneManager _sceneManager;
+
+    [Inject]
+    public void Construct(ISceneManager sceneManager) => 
+      _sceneManager = sceneManager;
+
+    private void Awake() => 
+      PlayButton.onClick.AddListener(Play);
+
+    protected override void OnEnable()
+    {
+      base.OnEnable();
+      Observe(_sceneManager.OnBeginSceneLoad);
+    }
+
+    async Task IOnBeginSceneLoad.Do()
+    {
+      PlayButton.interactable = false;
+      LoadingBar.SetActive(true);
+    }
+
+    private void Play() =>
+      _sceneManager.LoadSceneAsync(SceneName.Game);
+  }
+}
